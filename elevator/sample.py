b4985135a755e063a751a9c703158dfb3770490b
Robert Grey <rgrey@openinit.com>
2010-04-25 23:19:32 -0400

    add dynamic sample

--- elevator/sample.py
+++ elevator/sample.py
@@ -1,29 +1,43 @@
 from selenium import selenium
 import unittest, time, re
 
-class firstsuite(unittest.TestCase):
+class sample(unittest.TestCase):
     def setUp(self):
         self.verificationErrors = []
         self.selenium = selenium("localhost", 4444, "*chrome", "http://a810-bisweb.nyc.gov/")
         self.selenium.start()
     
-    def test_firstsuite(self):
+    def testSample(self):
         sel = self.selenium
         sel.open("/bisweb/bispi00.jsp")
+        # TODO make 'passdevicenumber' dynamic
         sel.type("passdevicenumber", "4p10707")
         sel.click("go26")
         sel.wait_for_page_to_load("30000")
-        sel.click("link=4075221")
+        sel.click("link=regexp:[0-9]{7}")
         sel.wait_for_page_to_load("30000")
         sel.click("link=Violations-ECB (DOB)")
-        sel.wait_for_page_to_load("30000")
-        sel.click("next")
-        sel.wait_for_page_to_load("30000")
-        sel.click("next")
-        sel.wait_for_page_to_load("30000")
-        sel.click("//tr[3]/td[2]/a/b")
-        sel.wait_for_page_to_load("30000")
+        while 1:
+            try: 
+                sel.wait_for_page_to_load("30000")
+                for i in range(1,8):
+                    id = sel.get_table("//table[3].%s.1" % i)
+                    status = sel.get_table("//table[3].%s.5" % i)
+                    print "id %s, status %s " % (id, status)
+                    if status == "CURED": self._processHit(id)
+                sel.click("next")
+            except Exception as e:
+                print "error or end of processing encountered"
+                #print e
+                break
     
+    def _processHit(self, id):
+        print "process %s" % id
+        sel = self.selenium
+        sel.click("link=%s" % id)
+        sel.wait_for_page_to_load("30000")
+        print "found %s" % sel.get_table("//table[3].6.2")
+
     def tearDown(self):
         self.selenium.stop()
         self.assertEqual([], self.verificationErrors)
